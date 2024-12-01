b4bcfa66a7f2008a4f37aab59425fe7942a184d6
Jim Hommes <jim@homm.es>
2024-12-01 19:33:15 +0100

    Moved counting of second list elements to initial data processing for optimization

--- Day1.py
+++ Day1.py
@@ -8,11 +8,19 @@ data_input = get_data(day=1, year=2024)
 # Processing data
 list1 = SortedList()
 list2 = SortedList()
+count_dict = dict()
 
 for line in data_input.split('\n'):
     numbers = re.findall(r'\d+', line)
     list1.add(int(numbers[0]))
     list2.add(int(numbers[1]))
+
+    if int(numbers[1]) not in count_dict.keys():
+        count_dict[int(numbers[1])] = 1
+    else:
+        count_dict[int(numbers[1])] += 1
+
+list1_set = set(list1)
 
 # Exercise 1
 distancelist = [abs(x - y) for (x, y) in zip(list1, list2)]
@@ -21,23 +29,10 @@ print(ex1_res)
 submit(ex1_res, part='a', day=1, year=2024)
 
 # Exercise 2
-count_dict = dict()
 ex2_res = 0
-while len(list1) > 0:
-    el1 = list1.pop()
+for el1 in list1_set:
     if el1 in count_dict.keys():
         ex2_res += el1 * count_dict[el1]
-    else:
-        el2 = list2.pop()
-        while el2 > el1:
-            el2 = list2.pop()
-        count = 0
-        while el2 == el1:
-            count += 1
-            el2 = list2.pop()
-        list2.add(el2)
-        ex2_res += el1 * count
-        count_dict[el1] = count
 
 print(ex2_res)
 submit(ex2_res, part='b', day=1, year=2024)
