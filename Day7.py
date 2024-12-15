26b3500f971b703d8beb990feb5f509eacb9a1f6
300702 <jim@homm.es>
2024-12-15 14:26:03 +0100

    Day 7

---
+++ Day7.py
@@ -0,0 +1,63 @@
+from aocd import *
+from time import perf_counter
+
+
+def concat_num(num1, num2):
+    return int(str(num1) + str(num2))
+
+
+def rec_evaluate_ex1(res, curr_value, nmbr_lst):
+    if len(nmbr_lst) == 1:
+        if res == curr_value + nmbr_lst[0]:
+            return True
+        else:
+            return res == curr_value * nmbr_lst[0]
+    else:
+        if rec_evaluate_ex1(res, curr_value + nmbr_lst[0], nmbr_lst[1:]):
+            return True
+        else:
+            return rec_evaluate_ex1(res, curr_value * nmbr_lst[0], nmbr_lst[1:])
+
+
+def rec_evaluate_ex2(res, curr_value, nmbr_lst):
+    if len(nmbr_lst) == 1:
+        if res == curr_value + nmbr_lst[0]:
+            return True
+        elif res == curr_value * nmbr_lst[0]:
+            return True
+        else:
+            return res == concat_num(curr_value, nmbr_lst[0])
+    else:
+        if rec_evaluate_ex1(res, curr_value + nmbr_lst[0], nmbr_lst[1:]):
+            return True
+        elif rec_evaluate_ex1(res, curr_value * nmbr_lst[0], nmbr_lst[1:]):
+            return True
+        else:
+            return rec_evaluate_ex1(res, concat_num(curr_value, nmbr_lst[0]), nmbr_lst[1:])
+
+
+data_input = get_data(day=7, year=2024)
+
+ex1_res = 0
+t1_start = perf_counter()
+for line in data_input.split('\n'):
+    evaluate_res = int(line.split(': ')[0])
+    numbers = list(map(int, line.split(': ')[1].split(' ')))
+    if rec_evaluate_ex1(evaluate_res, numbers[0], numbers[1:]):
+        ex1_res += evaluate_res
+t1_stop = perf_counter()
+
+print('Part A - Answer: ' + str(ex1_res) + ', calculated in ' + str((t1_stop - t1_start) * 1000) + ' ms')
+submit(int(ex1_res), part='a', day=7, year=2024)
+
+ex2_res = 0
+t2_start = perf_counter()
+for line in data_input.split('\n'):
+    evaluate_res = int(line.split(': ')[0])
+    numbers = list(map(int, line.split(': ')[1].split(' ')))
+    if rec_evaluate_ex2(evaluate_res, numbers[0], numbers[1:]):
+        ex2_res += evaluate_res
+t2_stop = perf_counter()
+
+print('Part B - Answer: ' + str(ex2_res) + ', calculated in ' + str((t2_stop - t2_start) * 1000) + ' ms')
+submit(int(ex2_res), part='b', day=7, year=2024)
